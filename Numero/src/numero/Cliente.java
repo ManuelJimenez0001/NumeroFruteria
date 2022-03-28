87548e7dd809a451b2b0a2405858b52d8fa7040a
Sami <smr0007@alu.medac.es>
2022-03-28 09:28:58 +0200

    Clase Cliente con el metodo constructor

---
+++ Numero/src/numero/Cliente.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numero;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+    private String nombre;
+    private int edad;
+    Boolean atendido = true;
+
+    public Cliente(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+
+
+    
+    
+    
+    
+    
+}
