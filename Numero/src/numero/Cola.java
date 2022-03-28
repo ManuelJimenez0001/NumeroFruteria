b245b2f593edd789e1de76342a0f621be3020dd8
Angelillo_glez <agg0022@alu.medac.es>
2022-03-28 10:05:30 +0200

    Clase Cola creada

---
+++ Numero/src/numero/Cola.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package numero;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ángel.García
+ */
+public class Cola {
+    ArrayList Cola;
+    
+    public Cola(){
+        Cola= new ArrayList();
+        
+    }
+}
