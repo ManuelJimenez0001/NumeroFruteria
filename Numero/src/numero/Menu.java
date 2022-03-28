349c2f3ff38d461f4f2ed996b2d47de953579cdd
ManuelJimenez0001 <mjt0001@alu.medac.es>
2022-03-28 10:16:24 +0200

    Inicio Menu

--- Numero/src/numero/Menu.java
+++ Numero/src/numero/Menu.java
@@ -32,6 +32,18 @@ public class Menu {
         
         String numero="";
         
+        System.out.println("Fruteria");
+        System.out.println("==================");
+        System.out.println("Boton 1: Añadir Cliente");
+        System.out.println("Boton 2: Atender Cliente");
+        System.out.println("Boton 3: Eliminar Cliente");
+        System.out.println("Boton 4: Avanzar Posicion");
+        System.out.println("Boton 5: Retroceder Posicion");
+        System.out.println("Boton 6: Dejar Pasar Cliente");
+        System.out.println("Boton 7: Clientes No Atendidos");
+        System.out.println("Boton 8: Clientes Atendidos");
+        System.out.println("Boton 9: Preferencia Mayores");
+        System.out.println("Boton 10: Salir");
         
       }
         
