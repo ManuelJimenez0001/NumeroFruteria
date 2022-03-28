742389fe6038ed5ea8dc27601beeb0a68ebf8282
ManuelJimenez0001 <mjt0001@alu.medac.es>
2022-03-28 09:39:34 +0200

    Inicio Menu

---
+++ Numero/src/numero/Menu.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ManuJimenez
+ */
+public class Menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner lector = new Scanner(System.in);
+        
+        final String añadirCliente= "AC";
+        final String atenderCliente="AtC";
+        final String eliminarCliente="EC";
+        final String avanzar= "AV";
+        final String retrasar="RE";
+        final String pasarCliente= "PC";
+        final String noAtendidos="NA";
+        final String atendidos="A";
+        final String mayores="M";
+        final String salir = "S";
+        
+        String numero="";
+        
+        
+      }
+        
+    }
+    
+
