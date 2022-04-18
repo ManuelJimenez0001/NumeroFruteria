d1a998224b7e09b5f5989bff598b0f066c223f71
Angelillo_glez <agg0022@alu.medac.es>
2022-04-18 10:41:57 +0200

    metodo atrasar y adelantar cliente

---
+++ Numero/test/numero/ColaTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package numero;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ángel.García
+ */
+public class ColaTest {
+    
+    public ColaTest() {
+    }
+
+    /**
+     * Test of nuevoCliente method, of class Cola.
+     */
+    @Test
+    public void testNuevoCliente() {
+    }
+
+    /**
+     * Test of despacharCliente method, of class Cola.
+     */
+    @Test
+    public void testDespacharCliente() {
+    }
+
+    /**
+     * Test of adelantarCliente method, of class Cola.
+     */
+    @Test
+    public void testAdelantarCliente() {
+    }
+    
+}
